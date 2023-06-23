136bb141ef71dca6b3ca959bd7e7727ada77b552
Olga Sivushkova <ya.olgapshenichnikova@yandex.ru>
2023-06-23 16:10:02 +0300

    Добавляем некоторые библиотеки

--- install.bat
+++ install.bat
@@ -2,12 +2,9 @@
 
 pip install notebook
 pip install jupyter
-pip install yfinance
+pip install geopandas
 pip install statsmodels
 pip install pandas_ta 
 pip install pandas_datareader
-pip install xlsxwriter
-pip install tensorflow
 pip install sklearn
-pip install qiskit
 pip install matplotlib
